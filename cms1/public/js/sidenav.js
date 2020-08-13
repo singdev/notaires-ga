53ba8840306173d599b83e875b906105414f8d69
Nve Hounkponou Orphée <norpheehounkponou@gmail.com>
2020-08-13 21:31:14 +0100

    sidebar style

--- cms1/public/js/sidenav.js
+++ cms1/public/js/sidenav.js
@@ -1,5 +1,26 @@
+loadMenu();
+loadRubrique();
 
-function showCreateForm(id){
-    document.querySelector(id).classList.add('show-create-form');
+function toggleShowForm(id){
+    document.querySelector(id).classList.toggle('show-create-form');
 }   
 
+function loadMenu(){
+    const menus = document.querySelectorAll('.menu');
+    menus.forEach(m => {
+        m.querySelector('.title').addEventListener('click', (e) => {
+            m.querySelector('.rubriques').classList.toggle('show-content');
+            m.querySelector('.hidden-form-ctn').classList.toggle('show-content');
+        })
+    })
+}
+
+function loadRubrique(){
+    const menus = document.querySelectorAll('.rubrique');
+    menus.forEach(m => {
+        m.querySelector('.title').addEventListener('click', (e) => {
+            m.querySelector('.articles').classList.toggle('show-content');
+            m.querySelector('.hidden-form-ctn').classList.toggle('show-content');
+        })
+    })
+}
