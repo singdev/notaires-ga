2e4c0fab64f57a56265b6b0d022ea5b2338c56fd
Nve Hounkponou Orphée <norpheehounkponou@gmail.com>
2020-09-09 15:26:32 +0100

    feat save

---
+++ site/public/js/article.js
@@ -0,0 +1,28 @@
+let editor = null;
+
+console.log("AAAAAAA");
+
+async function loadEditor(content) {
+    console.log("Editor");
+    editor = new EditorJS({
+        readonly: true,
+        tools: {
+            header: {
+                class: Header,
+                inlineToolbar: ['link']
+            },
+            embed: {
+                class: Embed,
+                inlineToolbar: false,
+                config: {
+                    services: {
+                        youtube: true,
+                        coub: true
+                    },
+                },
+            },
+        },
+        data: content
+    })
+}
+
